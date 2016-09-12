c481fef9a8606dffdfdc4a6d5d59097aa91a4e31
reresume <uconnrecenter@gmail.com>
2016-09-12 13:57:53 -0400

    WIP: Change mysql references to mssql

--- mysql.php
+++ mysql.php
@@ -1,17 +1,17 @@
 <?php
-    /* RE Server, PHP 5.3 */
+    /* RE Server, PHP 5.3 
     $mssql_host = 'sb1-sql12-1.business.uconn.edu';
     $mssql_database = 'REResume';
     $mssql_user = 'util.sql.reresume';
     $mssql_password = 'RE$RE$RE$299';
     $connection = mssql_connect($mssql_host, $mssql_user, $mssql_password);
-    mssql_select_db($mssql_database);
+    mssql_select_db($mssql_database);*/
 
-    /* Test Environment
+    /* Test Environment, PHP 5.6.24 */
     $serverName='sb1-sql12-1.business.uconn.edu';
     $user='util.sql.reresume';
     $pword='RE$RE$RE$299';
     $db='REResume';
-    $connection = mysqli_connect($serverName,$user,$pword,$db);
-    */
+    $connection = new PDO("sqlsrv:Server=$serverName;Database=$db",$user,$pword);
+    
 ?>
