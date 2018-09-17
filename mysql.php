473d420ed92d903f7d251554bd240592ea4e2716
JeffreyEng <JeffreyEng@Jeffreys-MacBook-Air-2.local>
2018-09-17 09:55:27 -0400

    Cleaning up git repository. Trying to sync the repository files with the current live site

--- mysql.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$mssql_host = 'sb1-sql12-1.business.uconn.edu';
-	$mssql_database = 'REResume';
-	$mssql_user = 'util.sql.reresume';
-	$mssql_password = 'RE$RE$RE$299';
-	$connection = mssql_connect( $mssql_host, $mssql_user, $mssql_password );
-	mssql_select_db( $mssql_database );
-?>
